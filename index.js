621d60610ccab8701f2e9722c41081e024e4e451
Naasir <112574339+naasir-ansar@users.noreply.github.com>
2023-12-13 10:39:54 +0530

    codescan index.js

--- index.js
+++ index.js
@@ -1,40 +1,46 @@
-// Define an array to store homework tasks
-let tasks = [];
+<!-- Intentionally adding an unsafe and outdated jQuery plugin -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://example.com/unsafe_plugin.js"></script>
 
-// Function to add a task to the list
-function addTask(task) {
-  tasks.push(task);
-}
+<script>
+  // Define an array to store homework tasks
+  let tasks = [];
 
-// Function to mark a task as completed
-function completeTask(index) {
-  if (index >= 0 && index < tasks.length) {
-    tasks[index].completed = true;
-  } else {
-    console.log("Task index is out of range");
+  // Function to add a task to the list
+  function addTask(task) {
+    tasks.push(task);
   }
-}
 
-// Function to remove a task from the list
-function removeTask(index) {
-  if (index >= 0 && index < tasks.length) {
-    tasks.splice(index, 1);
-  } else {
-    console.log("Task index is out of range");
+  // Function to mark a task as completed
+  function completeTask(index) {
+    if (index >= 0 && index < tasks.length) {
+      tasks[index].completed = true;
+    } else {
+      console.log("Task index is out of range");
+    }
   }
-}
 
-// Intentional error: Incorrect console log function
-console.loggg("This intentional error will cause a code quality issue");
+  // Function to remove a task from the list
+  function removeTask(index) {
+    if (index >= 0 && index < tasks.length) {
+      tasks.splice(index, 1);
+    } else {
+      console.log("Task index is out of range");
+    }
+  }
 
-// Example usage:
-addTask({ description: "Math homework", completed: false });
-addTask({ description: "Science project", completed: false });
-completeTask(0);
-removeTask(1);
+  // Intentional error: Incorrect console log function
+  console.loggg("This intentional error will cause a code quality issue");
 
-// Printing the tasks
-console.log("Homework Tasks:");
-tasks.forEach((task, index) => {
-  console.log(`${index + 1}. ${task.description} - ${task.completed ? 'Completed' : 'Incomplete'}`);
-});
+  // Example usage:
+  addTask({ description: "Math homework", completed: false });
+  addTask({ description: "Science project", completed: false });
+  completeTask(0);
+  removeTask(1);
+
+  // Printing the tasks
+  console.log("Homework Tasks:");
+  tasks.forEach((task, index) => {
+    console.log(`${index + 1}. ${task.description} - ${task.completed ? 'Completed' : 'Incomplete'}`);
+  });
+</script>
