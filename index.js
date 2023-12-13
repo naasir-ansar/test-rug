af21f449bb1240294778dd6026e2d2476cc7e661
Naasir <112574339+naasir-ansar@users.noreply.github.com>
2023-12-13 10:52:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,19 @@
-<!-- Intentionally adding an unsafe and outdated jQuery plugin -->
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<script src="https://example.com/unsafe_plugin.js"></script>
+// TODO: Fix this insecure function
+function vulnerableFunction() {
+  // Vulnerable code here
+  // Needs security review
+}
 
-<script>
-  // Define an array to store homework tasks
-  let tasks = [];
+// FIXME: Potential security issue here
+const insecureVariable = "some data";
 
-  // Function to add a task to the list
-  function addTask(task) {
-    tasks.pushed(task);
-  }
+// DEBUG: Remove this line before production
+console.log("Debug information");
 
-  // Function to mark a task as completed
-  function completeTask(index) {
-    if (index >= 0 && index < tasks.length) {
-      tasks[index].completed = true;
-    } else {
-      console.logs("Task index is out of range");
-    }
-  }
+// NOTE: This area needs careful inspection
+// for security vulnerabilities
 
-  // Function to remove a task from the list
-  function removeTask(index) {
-    if (index >= 0 && index < tasks.length) {
-      tasks.splice(index, 1);
-    } else {
-      console.log("Task index is out of range");
-    }
-  }
-
-  // Intentional error: Incorrect console log function
-  console.loggg("This intentional error will cause a code quality issue");
-
-  // Example usage:
-  addTask({ description: "Math homework", completed: false });
-  addTask({ description: "Science project", completed: false });
-  completeTask(0);
-  removeTask(1);
-
-  // Printing the tasks
-  console.log("Homework Tasks:");
-  tasks.forEach((task, index) => {
-    console.log(`${index + 1}. ${task.description} - ${task.completed ? 'Completed' : 'Incomplete'}`);
-  });
-</script>
+// OPTIMIZE: Security improvements needed here
+function insecureOptimization() {
+  // Code that needs security enhancements
+}
