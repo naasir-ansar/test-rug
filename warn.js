4e70f72241501d3f8ecd904d5a4b9ee191b59507
Naasir <112574339+naasir-ansar@users.noreply.github.com>
2024-01-10 17:15:22 +0530

    Create warn.js

---
+++ warn.js
@@ -0,0 +1,8 @@
+// Example of a hypothetical unsafe jQuery plugin (DO NOT USE)
+$.fn.unsafePlugin = function(userInput) {
+  // Unsafe usage of user input without proper validation
+  this.html("<h1>" + userInput + "</h1>");
+};
+
+// Usage of the unsafe plugin
+$("#targetElement").unsafePlugin("<script>alert('Vulnerable!')</script>");
